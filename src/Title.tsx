7d402c46804732c874358fad48f56823e964e224
jebikoh@gmail.com <jebikoh@MacBook-Pro.lan>
2021-09-02 19:26:30 -0400

    Add scaling prop to title text

--- src/Title.tsx
+++ src/Title.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
-export default function Title() {
+interface TitleProps {
+  scale?: number;
+}
+
+export default function Title({scale = 1}: TitleProps) {
   return (
     <Grid
       container
@@ -10,19 +14,26 @@ export default function Title() {
       direction="row"
       justifyContent="center"
       alignItems="center"
-      style={{backgroundColor: 'blue'}}
     >
       <Grid item xs={10}>
-        <Typography style={{fontSize: '22vw'}} variant="h1" component="h1">
+        <Typography
+          style={{fontSize: `${22 * scale}vw`}}
+          variant="h1"
+          component="h1"
+        >
           JAYDEN
         </Typography>
-        <Typography style={{fontSize: '25vw'}} variant="h1" component="h1">
+        <Typography
+          style={{fontSize: `${25 * scale}vw`}}
+          variant="h1"
+          component="h1"
+        >
           EDARA
         </Typography>
       </Grid>
       <Grid item xs={2}>
         <Typography
-          style={{fontSize: '8.7vw', textAlign: 'center'}}
+          style={{fontSize: `${8.7 * scale}vw`, textAlign: 'center'}}
           variant="h2"
         >
           JEBIKOH
