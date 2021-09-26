bbf489cc4c11a51f2705dc2a0750e31c8b255257
jebikoh@gmail.com <jebikoh@MacBook-Pro.local>
2021-09-26 14:22:11 -0700

    Add mobile only media query

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 
 [![Code Style: Google](https://img.shields.io/badge/code%20style-google-blueviolet.svg)](https://github.com/google/gts)
 
-Personal website version 3.
+Personal website version 3. Live testing branch with temporary modifications.
 
 ## To-Do
 
- - Media queries
- - Animations
- - Update projects section
+- Media queries
+- Animations
+- Update projects section
 
 ## Design
 
 ![Design Document](https://live.staticflickr.com/65535/51415788823_db09c13147_o.png)
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+import Typography from '@mui/material/Typography';
+
 import About from '../src/About';
 import Landing from '../src/Landing';
 import Projects from '../src/Projects';
 import Contact from '../src/Contact';
 
 export default function Index() {
+  const mobile = useMediaQuery('(max-width:400px');
   return (
+    // {mobile ? }
     <>
-      <Landing />
-      <About />
-      <Projects />
-      <Contact />
+      {!mobile && <Typography variant="h1">WIP</Typography>}
+      {mobile && <Landing />}
+      {mobile && <About />}
+      {mobile && <Projects />}
+      {mobile && <Contact />}
     </>
   );
 }
