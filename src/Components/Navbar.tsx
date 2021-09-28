30d83f9d65fc3ba4a77477fd72bfbc8329c6df89
jebikoh@gmail.com <jebikoh@MacBook-Pro.local>
2021-09-28 15:12:38 -0700

    Rename icon

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 // Icons
-import TwitterIcon from '@mui/icons-material/Twitter';
+import Twitter from '@mui/icons-material/Twitter';
 // import GithubIcon from '@mui/icons-material/Github';
 interface NavbarProps {
   height?: string;
@@ -17,10 +17,10 @@ export default function Navbar({height = '5vh'}: NavbarProps) {
       <Toolbar>
         <Box marginLeft="auto">
           <IconButton aria-label="Twitter" href="https://twitter.com/jebikoh">
-            <TwitterIcon />
+            <Twitter />
           </IconButton>
           <IconButton aria-label="Github" href="https://github.com/jebikoh">
-            <TwitterIcon />
+            <Twitter />
           </IconButton>
         </Box>
       </Toolbar>
